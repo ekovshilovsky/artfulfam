791566a7ac95cffb702a0b109f3c4af4a57ed08e
Eugene Kovshilovsky <4632199+ekovshilovsky@users.noreply.github.com>
2025-12-16 00:56:15 -0800

    Remove Vercel React Router package due to Hydrogen incompatibility

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,6 @@
 import type {Config} from '@react-router/dev/config';
 import {hydrogenPreset} from '@shopify/hydrogen/react-router-preset';
-import {vercelPreset} from '@vercel/react-router/vite';
 
 export default {
-  ssr: true,
-  presets: [hydrogenPreset(), vercelPreset()],
+  presets: [hydrogenPreset()],
 } satisfies Config;
